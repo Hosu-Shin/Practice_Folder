2135c480c9e38a4a45653df24e94695dde5be754
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-17 17:26:33 +0900

    Update menu_proc.php

--- Lunch/menu_proc.php
+++ Lunch/menu_proc.php
@@ -21,11 +21,34 @@
         return $result;
     }
 
-    $result = ins_menu($param);
-
-    if($result) {
-        header("Location: menu.php");
+    function sel_lunch(&$param)
+    {
+        $menu = $param["menu"];
+        $sql =
+            "   SELECT menu
+                FROM menu
+                WHERE menu = '$menu';
+            ";
+        $conn = get_conn();
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        return mysqli_fetch_assoc($result);
     }
+
+    $sel = sel_lunch($param);
+    if(empty($sel)) {
+        $result = ins_menu($param);
+        ?>
+        <script>
+            alert("<?=$insMenu?>이(가) 등록되었습니다.");
+            location.assign("menu.php");
+        </script>
+        <?php
+    } else {
+        ?>
+        <script>
+            alert("이미 등록된 메뉴입니다.");
+            location.assign("menu.php");
+        </script>
+<?php    } 
 ?>
-
-
