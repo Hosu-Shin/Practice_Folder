a0daf64b2a61da55caee2ba702518da9bcbaddef
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-22 10:30:01 +0900

    Update menu_proc.php

--- Lunch/menu_proc.php
+++ Lunch/menu_proc.php
@@ -6,6 +6,8 @@
     $param = [
         "menu" => $insMenu
     ];
+
+    $empty = preg_replace("/\s+/", "", $insMenu);
     
     $all_sel = sel_all_lunch();
 
@@ -17,7 +19,7 @@
             location.assign("menu.php");
         </script>
         <?php
-    } else if($insMenu == " ") { ?>
+    } else if(empty($empty)) { ?>
         <script>
             alert("내용을 입력해 주세요");
             location.assign("menu.php");
